00a397744f31bcd60012c0cdca8b6ee04a3cce05
orama254 <orama209@gmail.com>
2022-07-02 14:44:20 +0300

    feat: the initial commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">The Unicorn Startup</a>
         </h1>
 
         <p className={styles.description}>
@@ -23,30 +23,30 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2>About &rarr;</h2>
+            <p>a long text to describe something about the company</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2>Meet the team &rarr;</h2>
+            <p>a long text to describe something about the team we have here</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2>Careers &rarr;</h2>
+            <p>a long text to describe something about the Careers in the company</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2>Contact us &rarr;</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+            a long text to describe something about the how to contact us
             </p>
           </a>
         </div>
